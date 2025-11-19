c1001287cf817f77df17897ba4c6fa5eaffebdf8
Bernardo Vieira <bvieira@digitalub.ao>
2025-11-19 22:49:53 +0100

    Novas actulizações com Antigravity

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -3,3 +3,4 @@ from . import fe_payload_wizard
 from . import fe_get_state_wizard
 from . import fe_consult_invoice_wizard
 from . import fe_list_invoices_wizard
+from . import fe_solicitar_serie_wizard
